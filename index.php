3db3ad5e12dc6a2033212533b0ab72c634aca211
John Hasznosi <54283261+Noctiscreo@users.noreply.github.com>
2020-03-19 20:59:19 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+function validateId($patientId) {
+    if (!preg_match('/^\d+$/', $patientId)
+        || strlen($patientId) !== 10) {
+        return 'Patient ID must contain 10 numbers (no integers, no letters).';
+    } elseif (strlen($patientId) !== 10) {
+        return 'Field must have exactly 10 numbers.';
+    } elseif ($patientId === '1111111111'
+        || $patientId === '2222222222'
+        || $patientId === '3333333333'
+        || $patientId === '4444444444'
+        || $patientId === '5555555555'
+        || $patientId === '6666666666'
+        || $patientId === '7777777777'
+        || $patientId === '8888888888'
+        || $patientId === '9999999999'
+        || $patientId === '0000000000') {
+        return 'Field must not have the same number 10 times.';
+    } else {
+        return 'Patient ID accepted.';
+    }
+}
+
+echo validateId(1111111111);
---
+++ test/ValidateTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require('../index.php');
+
+class ValidateTest extends TestCase
+{
+    public function test_EmptyString()
+    {
+        $result = validateId('');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_Valid_1()
+    {
+        $result = validateId('4406283776');
+        $expected = 'Patient ID accepted.';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_TooFewDigits()
+    {
+        $result = validateId('123456');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_ContainsLetters()
+    {
+        $result = validateId('12312312S1');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_Valid_2()
+    {
+        $result = validateId('4277608332');
+        $expected = 'Patient ID accepted.';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_TooManyDigits()
+    {
+        $result = validateId('12312312312');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_SameDigits()
+    {
+        $result = validateId('2222222222');
+        $expected = 'Field must not have the same number 10 times.';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_Valid_3()
+    {
+        $result = validateId('2185273756');
+        $expected = 'Patient ID accepted.';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_NegativeNumber()
+    {
+        $result = validateId('-875123699');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_Decimal()
+    {
+        $result = validateId('58751236.9');
+        $expected = 'Patient ID must contain 10 numbers (no integers, no letters).';
+        $this->assertEquals($expected, $result);
+    }
+}
